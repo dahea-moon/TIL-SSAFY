f6ab3a588e983d872dccacea1f5779a18c214f03
Dahea Moon <dahea0512@gmail.com>
2019-10-07 15:04:38 +0900

    impolement create with mtv

--- 06_django_advanced/01_django_RECAP/board/urls.py
+++ 06_django_advanced/01_django_RECAP/board/urls.py
@@ -5,4 +5,14 @@ app_name = 'board'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('articles/', views.list, name='list'), # read 글 목록(list) render
+    path('articles/<int:id>/', views.detail, name='detail'), # read 글 상세보기(detail) render
+    
+    path('articles/new/', views.new, name='new'), # create 글 쓰기(new) render
+    path('articles/create/', views.create, name='create'), # create 글 저장(create)
+    
+    # path(''), # update 글 수정(edit) render
+    # path(''), # update 글 실제수정(update)
+    
+    # path(''), # delete 글 삭제(delete)
 ]
--- 06_django_advanced/01_django_RECAP/board/views.py
+++ 06_django_advanced/01_django_RECAP/board/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
-# Create your views here.
+
 def index(request):
     return render(request, 'board/index.html')
+
+
+def list(request):
+    articles = Article.objects.all()
+    context = {'articles': articles}
+    return render(request, 'board/list.html', context)
+
+
+def detail(request, id):
+    article = Article.objects.get(id=id)
+    context = {'article': article}
+    return render(request, 'board/detail.html', context)
+
+
+def new(request):
+    return render(request, 'board/new.html')
+
+
+def create(request):
+    article = Article()
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('board:detail', article.id)
